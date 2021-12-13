c38d67e1d0309630688d1fc46b493797547b26c5
ryely <ryelythompson@gmail.com>
2021-12-13 16:01:44 -0500

    nyo!

--- UwU-speech/UwU-speech.cpp
+++ UwU-speech/UwU-speech.cpp
@@ -16,20 +16,21 @@ std::string translate(std::string input)
 {
 
     system("cls");
-    std::string a,b,c,d,e,f,g;
+    std::string a,b,c,d,e,f,g,h;
     a = ReplaceAll(std::string(input), std::string("l"), std::string("w"));
     b = ReplaceAll(std::string(a), std::string("r"), std::string("w"));
     c = ReplaceAll(std::string(b), std::string("what"), std::string("wat"));
     d = ReplaceAll(std::string(c), std::string("?"), std::string("??"));           //This is a stupid idea. Too bad!
     e = ReplaceAll(std::string(d), std::string("!"), std::string("!!"));          
     f = ReplaceAll(std::string(e), std::string("idiot"), std::string("baka"));
-
+    g = ReplaceAll(std::string(f), std::string("no"), std::string("nyo"));
+    h = ReplaceAll(std::string(g), std::string("mo"), std::string("myo"));
     srand(time(NULL));
     int randNum = rand() % 3;
 
     std::string faceDictionary[8] = { ":3", "uwu", "owo","uwU","OwO","UwU","(uwu)","cX" };
-    f = f + " " + faceDictionary[randNum];
-    return(f);
+    h = h + " " + faceDictionary[randNum];
+    return(h);
 }
 
 void loop()
